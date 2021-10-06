1574a774f965617e09d07b4696cf008bee66b2dd
Porty-Eric <ericlesdsantos@gmail.com>
2021-10-06 12:32:03 -0300

    Delete this

--- polimorfismo.hs
+++ polimorfismo.hs
@@ -38,19 +38,3 @@ somaListaClasse (x : xs) = x + somaListaClasse xs
 contarIntervaloClasse :: (Enum a) => a -> a -> Int
 contarIntervaloClasse x y = length [x .. y]
 
-mapear :: (a -> a) -> [a] -> [a]
-mapear f [] = []
-mapear f (a : b) = (f a) : (mapear f b)
-
-
-reduzir :: [Int] -> Int
-reduzir [ ] = 0
-reduzir (x:xs) = x + reduzir xs
-
---Usando a função mapear e reduzir para uma lista de inteiros calcule o somatório do dobro dos elementos de uma lista de inteiros.
-somaDoDobro :: (Num a) => [a] -> a
-somaDoDobro [] = 0
-somaDoDobro (x:xs) = x*2 + somaDoDobro xs
-
-somaDoDobroProf :: [Int] -> Int
-somaDoDobroProf xs = reduzir (mapear (*2) xs)
